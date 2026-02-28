415af89fe42a21e0344bcaab4499bd9dfeb2148f
Sujal Bhagat <167618538+SujalBhagat2709@users.noreply.github.com>
2026-02-28 09:21:15 +0530

    Add files via upload

---
+++ 09_hyperparameter_tuning.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+
+X = pd.read_csv("features.csv")
+y = pd.read_csv("labels.csv")
+
+params = {"C": [0.1, 1, 10]}
+
+grid = GridSearchCV(
+    LogisticRegression(max_iter=1000),
+    param_grid=params,
+    cv=3
+)
+
+grid.fit(X, y.values.ravel())
+
+print("Best Parameters:", grid.best_params_)
+print("Best Score:", grid.best_score_)
